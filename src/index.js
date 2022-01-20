720f1da0bb285b13259e98e71bf53729edee320d
Giedre <giedre.klima@gmail.com>
2022-01-20 22:31:35 +0200

    load css with webpack, and images with webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import "./styles.css";
 
-const Greeting = () => <h2>Hello from React</h2>;
+const Greeting = () => {
+  return (
+    <div>
+      <h2>Hello from React</h2>
+      <div id="image"></div>
+    </div>
+  );
+};
 
 render(<Greeting />, document.getElementById("target"));
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [{ loader: "url-loader" }],
+      },
     ],
   },
 };
